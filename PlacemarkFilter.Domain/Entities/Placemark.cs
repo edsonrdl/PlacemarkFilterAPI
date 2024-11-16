91435935bff901a2ab37fb8991ccaad86bbec7df
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-11-16 15:23:13 -0300

    criado estrutura do projeto

---
+++ PlacemarkFilter.Domain/Entities/Placemark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlacemarkFilter.Domain.Entities
+{
+    internal class Placemark
+    {
+        public string Cliente { get; set; }
+        public string Situacao { get; set; }
+        public string Bairro { get; set; }
+        public string Referencia { get; set; }
+        public string RuaCruzamento { get; set; }
+    }
+}
